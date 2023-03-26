992d20ccd45303a3660f705ad984cfa6fac81e02
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-03-26 11:58:46 +0000

    Update ContentView.swift

--- NetworkRequestCombine/ContentView.swift
+++ NetworkRequestCombine/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     var body: some View {
@@ -23,4 +24,33 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct User: Decodable {
+    let id: Int
+    let name: String
+}
+
+struct Post: Decodable {
+    let id: Int
+    let userId: String
+    let title: String
+}
+
+struct Comment: Decodable {
+    let id: String
+    let postId: String
+    let email: String
+}
+
+
+struct CombineImp {
+    
+    func getUsers() -> AnyPublisher<[User], Error> {
+        
+    }
+}
+
+struct AwaitAsyncImp {
+    
 }
