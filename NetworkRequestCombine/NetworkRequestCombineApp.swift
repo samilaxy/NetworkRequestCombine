743cffa785b47fdb43a9cf64d08628b64eb1981a
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-03-26 11:38:59 +0000

    Initial Commit

---
+++ NetworkRequestCombine/NetworkRequestCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkRequestCombineApp.swift
+//  NetworkRequestCombine
+//
+//  Created by Noye Samuel on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkRequestCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
